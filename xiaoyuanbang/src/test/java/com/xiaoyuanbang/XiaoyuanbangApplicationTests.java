2f675ca7675d47e25750e9d19324fd3737c665bc
spx <457799096@qq.com>
2019-05-09 13:35:47 +0800

    origin version

---
+++ xiaoyuanbang/src/test/java/com/xiaoyuanbang/XiaoyuanbangApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiaoyuanbang;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XiaoyuanbangApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
