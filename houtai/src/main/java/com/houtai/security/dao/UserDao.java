5e49df5eda9b5954f39ce799c2d086a8be02b264
kylesun <457799096@qq.com>
2019-11-15 19:04:48 +0800

    houtai

---
+++ houtai/src/main/java/com/houtai/security/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.houtai.security.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface UserDao {
+}
